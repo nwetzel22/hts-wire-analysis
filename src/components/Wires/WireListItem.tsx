a444b969c08faec6d167d97c6fd2efc4d3192f05
Nicholas Wetzel <wetzel_nicholas@bah.com>
2023-01-17 21:06:58 -0500

    Implement wire delete

--- src/components/Wires/WireListItem.tsx
+++ src/components/Wires/WireListItem.tsx
@@ -1,4 +1,6 @@
-import { ListItem, ListItemButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button, ListItem, ListItemButton } from "@mui/material";
+import { ComponentPropsWithoutRef } from "react";
 import { Wire } from "./WireList";
 
 const dateFormatter = (d: Date) => {
@@ -9,12 +11,30 @@ const dateFormatter = (d: Date) => {
   });
 };
 
-const WireListItem = (wire: Wire) => {
+interface WireListItemProps extends ComponentPropsWithoutRef<"div"> {
+  wire: Wire;
+  deleteHandler: (wire: Wire) => void;
+}
+
+const WireListItem = (props: WireListItemProps) => {
+  const onDeleteClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    props.deleteHandler(props.wire);
+  };
+
   return (
     <ListItem sx={{ p: 0 }}>
       <ListItemButton sx={{ display: "flex", justifyContent: "space-between" }}>
-        <div>{wire.title}</div>
-        <div>{dateFormatter(wire.date)}</div>
+        <div>
+          <div>{props.wire.title}</div>
+          <div style={{fontSize: '0.7rem'}}>{dateFormatter(props.wire.date)}</div>
+        </div>
+        <Button
+          onClick={onDeleteClick}
+          variant="outlined"
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+        </Button>
       </ListItemButton>
     </ListItem>
   );
