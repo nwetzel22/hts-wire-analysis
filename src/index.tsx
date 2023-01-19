7ba06e993e99529be2d9eab755c694fd24615918
Nicholas Wetzel <wetzel_nicholas@bah.com>
2023-01-18 23:04:59 -0500

    Configure amplify in root

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,10 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
